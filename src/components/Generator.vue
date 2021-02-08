f91eb1e5dde48907ab1d53c9e7a815f8c124ce00
Timothée Varaillas <timothee.varaillas@gmail.com>
2021-02-08 10:32:56 +0100

    Amélioration du moteur de génération de mot de passe

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -23,22 +23,6 @@
           </v-col>
         </v-row>
       </v-card-text>
-      
-      <v-card-text>
-        <v-row>
-          <v-col>
-            <v-slider
-              label="Minuscules"
-              v-model="lower"
-              :max="length"
-              min="0"
-            ></v-slider>
-          </v-col>
-          <v-col>
-            <span>{{ lower }}</span>
-          </v-col>
-        </v-row>
-      </v-card-text>
 
       <v-card-text>
         <v-row>
@@ -61,13 +45,13 @@
           <v-col>
             <v-slider
               label="Chiffres" 
-              v-model="number"
+              v-model="digit"
               :max="length"
               min="0"
             ></v-slider>
           </v-col>
           <v-col>
-            <span>{{ number }}</span>
+            <span>{{ digit }}</span>
           </v-col>
         </v-row>
       </v-card-text>
@@ -88,8 +72,6 @@
         </v-row>
       </v-card-text>
 
-      
-
       <v-alert class="ma-2 mb-6" color="grey lighten-3">
         <span>{{ id }}</span>
       </v-alert>
@@ -108,43 +90,98 @@
 import shuffle from 'lodash/shuffle';
 
 export default {
-  name: 'MathRandom',
+  name: 'Generator',
   data: () => ({
     id: '',
-    length: 16,
-    upper: 4,
-    lower: 4,
-    number: 5,
-    symbol: 4
+    length: 10,
+    lower: 10,
+    upper: 0,
+    digit: 0,
+    symbol: 0
   }),
   mounted() {
     this.generate();
   },
   watch: {
     length() {
-      this.generate();
-    },
-    lower() {
+      if (this.total < this.length) {
+        this.increment()
+      }
+      if (this.total > this.length) {
+        this.decrement();
+      }
       this.generate();
     },
     upper() {
+      if (this.total < this.length) {
+        this.increment();
+      }
+      if (this.total > this.length) {
+        this.decrement('upper');
+      }
       this.generate();
     },
-    number() {
+    digit() {
+      if (this.total < this.length) {
+        this.increment();
+      }
+      if (this.total > this.length) {
+        this.decrement('digit');
+      }
       this.generate();
     },
     symbol() {
+      if (this.total < this.length) {
+        this.increment();
+      }
+      if (this.total > this.length) {
+        this.decrement('symbol');
+      }
       this.generate();
     },
   },
+  computed: {
+    total() {
+      return this.lower + this.upper + this.digit + this.symbol;
+    }, 
+    maxValue() {
+      return [ 
+        { label: 'upper', value: this.upper }, 
+        { label: 'digit', value: this.digit }, 
+        { label: 'symbol', value: this.symbol }
+      ].sort((a, b) => {
+        return b.value - a.value
+      });
+    }
+  },
   methods: {
+    increment() {
+      while (this.total != this.length) {
+        this.lower ++;
+      }
+    },
+    decrement(charType = null) {
+      while(this.total != this.length) {
+        if (this.lower > 0) {
+          this.lower--;
+        }
+        else {
+          if (charType != this.maxValue[0].label) {
+            this[this.maxValue[0].label]--;
+          }
+          else {
+            this[this.maxValue[1].label]--;
+          }
+        }
+      }
+    },
     getRandomUpperCase() {
       return String.fromCharCode(Math.floor(Math.random()*26)+65);
     },
     getRandomLowerCase() {
       return String.fromCharCode(Math.floor(Math.random()*26)+97);
     },
-    getRandomNumber(){
+    getRandomDigit(){
       return String.fromCharCode(Math.floor(Math.random()*10)+48);
     },
     getRandomSymbol(){
@@ -159,8 +196,8 @@ export default {
       for (let i=0; i<this.lower; i++) {
         characters.push(this.getRandomLowerCase());
       }
-      for (let i=0; i<this.number; i++) {
-        characters.push(this.getRandomNumber());
+      for (let i=0; i<this.digit; i++) {
+        characters.push(this.getRandomDigit());
       }
       for (let i=0; i<this.symbol; i++) {
         characters.push(this.getRandomSymbol());
