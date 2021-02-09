153017e767874ad162677219d4badd2db1f02562
Timothée Varaillas <timothee.varaillas@gmail.com>
2021-02-09 10:36:17 +0100

    Création d'un dictionnaire multilangues

--- src/components/password-generators/Words.vue
+++ src/components/password-generators/Words.vue
@@ -5,34 +5,33 @@
 </template>
 
 <script>
-import words from '../../assets/words_fr.json';
 import shuffle from 'lodash/shuffle';
+import dictionary from '../../dictionaries';
 
 export default {
   name: 'Characters',
   data: () => ({
     password: '',
     nbWords: 10,
-    separator: '.'
+    separator: '.',
   }),
   mounted() {
     this.generatePassword();
   },
   methods: {
-    generatePassword(){
-      const dic = shuffle(words)
-        .filter(word => word.length > 4)
-        .filter(word => {
-          var format = /[ `!@#$%^&*()_+\-=[\]{};':"\\|,.<> /?~]/;  
+    generatePassword() {
+      const dic = shuffle(dictionary.fr)
+        .filter((word) => word.length > 4)
+        .filter((word) => {
+          const format = /[ `!@#$%^&*()_+\-=[\]{};':"\\|,.<> /?~]/;
           return !format.test(word);
         })
-        .map(word => word.toLowerCase())
-        .map(word => word.normalize("NFD").replace(/[\u0300-\u036f]/g, "")); 
+        .map((word) => word.toLowerCase())
+        .map((word) => word.normalize('NFD').replace(/[\u0300-\u036f]/g, ''));
       this.password = dic.slice(0, this.nbWords).join(this.separator);
-
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss">
---
+++ src/dictionaries.js
@@ -0,0 +1,7 @@
+import FR from './assets/words_fr.json';
+import EN from './assets/words_en.json';
+
+export default {
+  fr: FR,
+  en: EN,
+};
